91e152a478059cbdb2a6700659ab221285a4ecc7
Stefan Glaisner <stefan.glaisner@student.uni-tuebingen.de>
2023-10-22 22:20:37 +0200

    added unit tests for add_intercept and nlls_weights

---
+++ tests/testthat/test-nnls_weights.R
@@ -0,0 +1,28 @@
+test_that("nnls_weights produces correct weights", {
+  set.seed(42)
+  n = 1000
+  p = 5
+  X = matrix(runif(n*p), ncol=p)
+  true_weights = c(0.3, 0, 0, 0.1, 0.6)
+  y = X %*% true_weights + rnorm(n, sd = 0.1)
+
+  estimated_weights = nnls_weights(X, y)
+
+  expect_equal(estimated_weights, true_weights, tolerance=0.05)
+})
+
+test_that("nnls_weights produces correct weights when features are highly correlated", {
+  library(mvtnorm)
+  set.seed(42)
+  n = 1000
+  p = 8
+  cov_mat = matrix(rep(0.9, p*p), ncol=p, nrow=p)
+  cov_mat[cbind(1:p,1:p)] = 1
+  X = mvtnorm::rmvnorm(n=n, mean=rep(0, p), sigma=cov_mat)
+  true_weights = c(0.05, 0.05, 0.1, 0, 0.3, 0.3, 0.2, 0)
+  y = X %*% true_weights + rnorm(n, sd = 0.1)
+
+  estimated_weights = nnls_weights(X, y)
+
+  expect_equal(estimated_weights, true_weights, tolerance=0.05)
+})
