9d2fb897e106a6ec16eed6673965729972dd081c
Stefan Glaisner <stefan.glaisner@student.uni-tuebingen.de>
2023-11-15 18:32:05 +0100

    added real data from DoubleML dgp

--- assets/nuisance_data_create.R
+++ assets/nuisance_data_create.R
@@ -1,2 +1,18 @@
 # package data
+library(DoubleML)
+library(dplyr)
+n = 5000
+p = 10
+alpha = 0.2
+nuisance_data = DoubleML::make_plr_CCDDHNR2018(
+  n_obs = n,
+  dim_x = p,
+  alpha = alpha,
+  return_type = "data.frame"
+)
+nuisance_data = nuisance_data %>%
+  rename(w = d) %>%
+  rename_with(tolower) %>%
+  relocate(y, w) %>%
+  as.matrix()
 save(nuisance_data , file = "data/nuisance_data.RData", version = 2)
